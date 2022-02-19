58697b1dd2f335329f6b4882b91f0d8fd6611e32
AG <abbfzl@gmail.com>
2022-02-20 01:16:04 +0330

    Initial commit

---
+++ src/main/java/com/blockra/blockra/BlockraApplication.java
@@ -0,0 +1,13 @@
+package com.blockra.blockra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockraApplication.class, args);
+	}
+
+}
